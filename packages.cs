bf42665769b78a382ed68a8479ff7725446d995e
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2015-03-24 23:05:11 -0700

    Remove debug messages

--- packages.cs
+++ packages.cs
@@ -2,23 +2,12 @@ package PLYRARCH_package
 {
 	function secureClientCmd_ClientJoin(%name, %objid, %bl_id, %var1, %var2, %admin, %superadmin)
 	{
-		echo("secureClientCmd_ClientJoin() has been called...");
-		echo("%name:" SPC %name);
-		echo("%objid:" SPC %objid);
-		echo("%bl_id:" SPC %bl_id);
-		echo("%var1:" SPC %var1);
-		echo("%var2:" SPC %var2);
-		echo("%admin:" SPC %admin);
-		echo("%superadmin:" SPC %superadmin);
 		PLYRARCH_addPlayer(%bl_id, %name);
 		parent::secureClientCmd_ClientJoin(%name, %objid, %bl_id, %var1, %var2, %admin, %superadmin);
 	}
 	
 	function secureClientCmd_ClientDrop(%name, %objid)
 	{
-		echo("secureClientCmd_ClientDrop");
-		echo("%name:" SPC %name);
-		echo("%objid:" SPC %objid);
 		parent::secureClientCmd_ClientDrop(%name, %objid);
 	}
 };
