b4c19201d6171aca43342f2a7e8c067a8914b82c
Joshua Brockschmidt <JoshuaBrockschmidt@gmail.com>
2015-03-25 21:29:14 -0700

    Change write directory

--- config.cs
+++ config.cs
@@ -1,5 +1,5 @@
 // This is the filepath of the file raw data will be written to
-$PLYRARCH::rawArchiveFile = $PLYRARCH::directory @ "/player_archive_raw.txt";
+$PLYRARCH::rawArchiveFile = "config/client/playerArchive/player_archive_raw.txt";
 
 // This is the filepath of the file organized data will be written to
-$PLYRARCH::archiveFile = $PLYRARCH::directory @ "/player_archive.txt";
+$PLYRARCH::archiveFile = "config/client/playerArchive/player_archive.txt";
